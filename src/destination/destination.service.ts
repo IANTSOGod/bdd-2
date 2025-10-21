1f0d727fac1d908196505cca8f59ed2d5c59c844
IANTSOGod <iantsochristianrazafindrazaka@gmail.com>
2025-10-21 09:35:34 +0300

    fix after models

--- src/destination/destination.service.ts
+++ src/destination/destination.service.ts
@@ -31,7 +31,6 @@ export class DestinationService {
             description: true,
             price: true,
             discount: true,
-            duration: true,
             rating: true,
             destination: false,
           },
